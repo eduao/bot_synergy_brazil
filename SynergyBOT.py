64d41f961718df8f99cdd382c627f62f45d00d30
Eduardo A. O <emaildoedu00@gmail.com>
2018-04-20 12:36:27 -0300

    Inserido verificações de prefixo antes de realizar o comando

--- SynergyBOT.py
+++ SynergyBOT.py
@@ -9,54 +9,70 @@ import json
 datas_json_auth = json.load(open('auth.json'))
 
 Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "?") #Initialise client bot
-canal_id = str(datas_json_auth["channel_id"]) # Obtain the id from default channel to the bot chat in
-database_connection = datas_json_auth["connection_db"] # Get the configurations to access the database
+prefix = 's!'
+client = commands.Bot(command_prefix=prefix) #Initialise client bot
+#bot = commands.Bot(command_prefix='?', description=description)
+canal_id = str(datas_json_auth['channel_id']) # Obtain the id from default channel to the bot chat in
+database_connection = datas_json_auth['connection_db'] # Get the configurations to access the database
+
+def set_prefix(value):
+    global prefix
+    prefix = value
+
+def get_prefix():
+    return prefix
 
 @client.event
 async def on_ready():
     print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
 
+@client.command()
+async def teste(ctx, arg1, arg2):
+    await ctx.send('You passed {} and {}'.format(arg1, arg2))
+
+@client.command(name = 'add')
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
 @client.event
 async def on_message(message):
     if canal_id: # Check if the var canal_id is setted
-        if message.content.startswith('s!comandos'):
-            comandos = "Oi "+message.author.mention+" segue a lista de comandos disponíveis atualmente:\n"
-            comandos += "s!cookie -> Bot envia um emoji de cookie\n"
-            comandos += "por enquanto é só :D"
-            await client.send_message(discord.Object(id=canal_id), comandos)
-        if message.content.startswith('s!cookie'): # Command cookie to print the emoticon cookie
-            await client.send_message(discord.Object(id=canal_id), ":cookie:") #responds with Cookie emoji when someone says "cookie"
-        elif message.content == "$tipos_jogos":
-            await client.send_message(discord.Object(id=canal_id), message.author.mention+", estamos analisando nossos dados e isso é o que temos pra hoje :microphone:") #responds with Cookie emoji when someone says "cookie"
-            db = MySQLdb.connect(host=database_connection["host"],# your host, usually localhost
-                                 user=database_connection["user"],# your username
-                                 passwd=database_connection["passwd"],# your password
-                                 db=database_connection["db_name"])
-            # you must create a Cursor object. It will let
-            #  you execute all the queries you need
-            cur = db.cursor()
-            # Use all the SQL you like
-            cur.execute("SELECT name FROM type_game")
-            mensagem = "Infelizmente não está cadastrado nenhum tipo de jogo atualmente"
-            if len(cur.fetchall()) > 0:
-                # print all the first cell of all the rows
-                for row in cur.fetchall():
-                    await client.send_message(discord.Object(id=canal_id), row[0]+" putsss")
+        if message.content.startswith(get_prefix()):
+            cmd = message.content[len(get_prefix()):]
+            if cmd:
+                # await client.send_message(discord.Object(id=canal_id), cmd)
+                if cmd.startswith('mudar_pref'):
+                    novo_pref = cmd[len('mudar_pref'):]
+                    set_prefix(novo_pref)
+                    await client.send_message(discord.Object(id=canal_id), get_prefix())
+                if cmd.startswith('comandos'):
+                    comandos = 'Oi '+message.author.mention+' segue a lista de comandos disponíveis atualmente:\n'
+                    await client.send_message(discord.Object(id=canal_id), comandos)
+                    comandos = '```Markdown \n'
+                    comandos += '# '+prefix+'comandos: \n'
+                    comandos += '[s!cookie](Bot envia um emoji de cookie)\n'
+                    comandos += '[s!qeb <nome>] ou [s!bixao <nome>] ou [s!bixão <nome>](Diga quem é o bixão do server)\n'
+                    comandos += 'por enquanto é só :D'
+                    comandos += '```'
+                    await client.send_message(discord.Object(id=canal_id), comandos)
+                elif cmd.startswith('cookie'): # Command cookie to print the emoticon cookie
+                    await client.send_message(discord.Object(id=canal_id), ":cookie:") #responds with Cookie emoji when someone says "cookie"
+                elif cmd.startswith('qeb') or cmd.startswith('bixao') or cmd.startswith('bixão'):
+                    passp = 'bixão'
+                    if cmd.startswith('qeb'):
+                        passp = 'qeb'
+                    elif cmd.startswith('bixao'):
+                        passp = 'bixao'
+                    name = cmd[len(passp):].strip()
+                    if name:
+                        await client.send_message(discord.Object(id=canal_id), '{} é o bixão meeeemo heeeeeein!!!!'.format(name))
+                    else:
+                        await client.send_message(discord.Object(id=canal_id), 'coloque '+get_prefix()+passp+' <nome do bixão>')
+                        
             else:
-                print(message.author.id)
-                await client.send_message(discord.Object(id=canal_id), " Opa, "+ message.author.mention +" : "+mensagem)
-            
-            db.close()
-        if message.content.startswith('$bixao'):
-            await client.send_message(discord.Object(id=canal_id), 'Quem é o bixão? Escreva $nome_do_bixao nome')
-            def check(msg):
-                return msg.content.startswith('$nome_do_bixao')
-            message = await client.wait_for_message(author=message.author, check=check)
-            name = message.content[len('$nome_do_bixao'):].strip()
-            await client.send_message(discord.Object(id=canal_id), '{} é o bixão meeeemo heeeeeein!!!!'.format(name))
-        if message.content.startswith('$CANAL'):
-            await client.send_message(discord.Object(id=canal_id), 'hello')
+                await client.send_message(discord.Object(id=canal_id), 'entrou com comando vazio')
+        else:
+            return
     else:
         print("Vazio");
         exit();
